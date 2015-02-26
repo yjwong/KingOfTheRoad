02ed4139a66a5b35afcef6b9d43a4d0e1ba0859b
yukithesnowman <founders2008@hotmail.com>
2015-02-26 19:34:55 +0800

    Created Drawer Layout

--- app/src/main/java/com/ejay/kingoftheroad/MainActivity.java
+++ app/src/main/java/com/ejay/kingoftheroad/MainActivity.java
@@ -2,12 +2,22 @@ package com.ejay.kingoftheroad;
 
 import android.app.FragmentTransaction;
 import android.bluetooth.BluetoothGatt;
+import android.content.res.Configuration;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -47,7 +57,10 @@ public class MainActivity extends ActionBarActivity {
     private boolean mNabuLiveFitnessEnabled;
     protected GoogleMap mMap;
 
-
+    private String[] drawerListViewItems;
+    private DrawerLayout drawerLayout;
+    private ListView drawerListView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +93,60 @@ public class MainActivity extends ActionBarActivity {
             nabuGetAllBandList();
         }
 
+        // get list items from strings.xml
+        drawerListViewItems = getResources().getStringArray(R.array.items);
+        // get ListView defined in activity_main.xml
+        drawerListView = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        drawerListView.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, drawerListViewItems));
+
+        // 2. App Icon
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // 2.1 create ActionBarDrawerToggle
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        );
+
+        // 2.2 Set actionBarDrawerToggle as the DrawerListener
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        // 2.3 enable and show "up" arrow
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // just styling option
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        drawerListView.setOnItemClickListener(new DrawerItemClickListener());
+
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            Toast.makeText(MainActivity.this, ((TextView) view).getText(), Toast.LENGTH_LONG).show();
+            drawerLayout.closeDrawer(drawerListView);
+
+        }
     }
 
     @Override
